cf2c1dce4499b0791290f33e16c375549c8189e6
Julien Birgand <julien.birgand@gmail.com>
2021-06-03 08:19:51 +0200

    test

--- pages/portfolio/[category].tsx
+++ pages/portfolio/[category].tsx
@@ -21,7 +21,7 @@ const Portfolio: FunctionComponent<Props> = (props) => {
         <title>Portfolio</title>
       </Head>
       <Container>
-        <div className="flex justify-center mb-7">
+        <div className="flex justify-center mb-8">
           <ul className="flex justify-center flex-wrap gap-x-9">
             <PortfolioFilter category={CATEGORY_ALL} />
             {categories.map((category) => (
